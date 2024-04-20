f86049aa8163f0edbb9f2af1fc115e91716eea76
Sophia Peckner <sophiapeckner@gmail.com>
2024-04-20 16:11:11 -0700

    configure Gemini API into webpage

--- LAHACKS/pages/index.py
+++ LAHACKS/pages/index.py
@@ -4,7 +4,14 @@ from LAHACKS import styles
 from LAHACKS.templates import template
 
 import reflex as rx
+import os
+import pathlib
+import tqdm
+from dotenv import load_dotenv
+import google.generativeai as genai
 
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 @template(route="/", title="Home")
 def index() -> rx.Component:
@@ -13,6 +20,18 @@ def index() -> rx.Component:
     Returns:
         The UI for the home page.
     """
-    with open("README.md", encoding="utf-8") as readme:
-        content = readme.read()
-    return rx.markdown(content, component_map=styles.markdown_style)
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content("Write a story about a magic backpack.")
+    
+    return rx.vstack(
+        rx.heading("Dashboard", size="8"),
+        rx.text("Welcome to Reflex!"),
+        rx.text(response.text),
+    )
+
+def convert_pdf_to_text():
+    pass
+
+def check_dosage():
+    pass
