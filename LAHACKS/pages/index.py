b6b6d86520ce230c6fca64160f1ca19b9b7c6cc3
Anahita Vaidhya <anahitavaidhya@Anahitas-MacBook-Pro.local>
2024-04-21 04:06:09 -0700

    added page content to dashboard

--- LAHACKS/pages/index.py
+++ LAHACKS/pages/index.py
@@ -13,7 +13,17 @@ def index() -> rx.Component:
     Returns:
         The UI for the home page.
     """
+
     return rx.vstack(
         rx.heading("Dashboard", size="8"),
-        rx.text("Welcome to Reflex!"),
-    )
+        rx.text("How to Use the Chat"),
+        rx.flex(
+            rx.card(rx.markdown("**Prescription Feedback** \n\n Based on diagnosis and/or medical records, identify potential mistakes with the prescription given \n\n **Recommended Information to Input:** medical records, doctor’s notes, doctor’s diagnosis."), size="5", color="black"),
+            rx.card(rx.markdown("**Prescription Verification** \n\n Based on diagnosis and prescription given, verify the medicine received is actually what the doctor prescribed. \n\n **Recommended Information to Input:** medical records, doctor’s notes, doctor’s prescription, image of medicine received."), size="5", color="black"),
+            rx.card(rx.markdown("**Dosage Feedback** \n\n Based on diagnosis and/or medical records, provide feedback on the dosage amount. \n\n **Recommended Information to Input:** medical records, doctor’s notes, doctor’s diagnosis"), size="5", color="black"),
+            rx.card(rx.markdown("**Explain My Medication** \n\n Based on diagnosis and/or medical records, prescription and dosage amount, explain what the medicine does simply and how to take it. \n\n **Recommended Information to Input:** medical records, doctor’s notes, doctor’s diagnosis, image of medicine received."), size="5", color="black"),
+            spacing="2",
+            align_items="flex-start",
+            flex_wrap="wrap",
+        )
+    ),
